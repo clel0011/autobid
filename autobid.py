5c1685f9928f3f3e94deb8c94f9d926e0dfca403
clel0011 <clel0011@umn.edu>
2021-02-13 12:54:20 -0600

    Outline

---
+++ autobid.py
@@ -0,0 +1,19 @@
+#Purpose: make best bid taking into account cards in hand, previous bids, possibly score
+'''
+Inputs:
+    Hand: 2D array
+        Integers from 0-51
+    Incoming Bids: 2D array
+        First index is name of bidder
+        Second index is string representing the name of the bid i.e. 1 No Trump
+Returns: "best" bid for current situation in the form of the original input (2D array or string)
+'''
+def autoBid(hand, incomingBids):
+    outgoingBid = 
+
+
+
+
+    return outgoingBid
+
+print(autoBid(0, 0))
